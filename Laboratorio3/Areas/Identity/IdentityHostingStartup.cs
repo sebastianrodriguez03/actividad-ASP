905c0a47d9ef615cb53d1d2eff68432af19c5f09
sebastianrodriguez <sebastianrodriguez0303@gmail.com>
2019-06-21 20:17:55 -0500

    Initial commit

---
+++ Laboratorio3/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Laboratorio3.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Laboratorio3.Areas.Identity.IdentityHostingStartup))]
+namespace Laboratorio3.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
